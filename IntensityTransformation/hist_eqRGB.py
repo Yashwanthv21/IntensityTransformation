16bc230b0ac387d0f8b30885f22f8429d185c3e8
Suchismitha Vedala <suchismithavedala@Suchismithas-MacBook-Pro.local>
2017-12-08 18:07:41 -0600

    v9

---
+++ IntensityTransformation/hist_eqRGB.py
@@ -0,0 +1,58 @@
+import numpy as np
+import cv2
+
+img=cv2.imread("/Users/suchismithavedala/Desktop/Fall 2017/DIP/Assignment 2/assignment-2-Suchismitha-Vedala/Lenna0.jpg")
+
+
+M,N,c=img.shape
+histx=histy=histz=[0 for i in range(256)]
+for i in range(M):
+    for j in range(N):
+        histx[img[i,j,0]]=histx[img[i,j,0]]+1
+        histy[img[i,j,1]]=histy[img[i,j,1]]+1
+        histz[img[i,j,2]]=histz[img[i,j,2]]+1
+cdfx = [sum(histx[:i+1]) for i in range(len(histx))]
+cdfy =[sum(histy[:i+1]) for i in range(len(histy))]
+cdfz =[sum(histz[:i+1]) for i in range(len(histz))]
+cdfx1=cdfy1=cdfz1=[0 for i in range(256)]
+mx1 = min(i for i in cdfx if i > 0)
+my1 = min(i for i in cdfy if i > 0)
+mz1 = min(i for i in cdfz if i > 0)
+mx2=np.max(cdfx)
+my2=np.max(cdfy)
+mz2=np.max(cdfz)
+cx=mx2-mx1
+cy=my2-my1
+cz=mz2-mz1
+for i in range(256):
+    if(cdfx[i]>0):
+        cdfx1[i]=(cdfx[i]-mx1)*255/(cx)
+    else:
+        cdfx1[i]=cdfx[i]
+for i in range(256):
+    if(cdfy[i]>0):
+        cdfy1[i]=(cdfy[i]-my1)*255/(cy)
+    else:
+        cdfy1[i]=cdfy[i]
+for i in range(256):
+    if(cdfz[i]>0):
+        cdfz1[i]=(cdfz[i]-mz1)*255/(cz)
+    else:
+        cdfz1[i]=cdfz[i]
+
+new_img=np.zeros((int(M),int(N),int(c)),np.uint8)
+for i in range(M):
+    for j in range(N):
+        new_img[i, j,0] = cdfx1[img[i, j,0]]
+        new_img[i, j,1] = cdfy1[img[i, j,1]]
+        new_img[i, j,2] = cdfz1[img[i, j,2]]
+
+equalized_hist=[0 for i in range(256)]
+for i in range(M):
+    for j in range(N):
+        equalized_hist[new_img[i,j,0]]=equalized_hist[new_img[i,j,0]]+1
+
+
+cv2.namedWindow("B")
+cv2.imshow("B", new_img)
+cv2.waitKey(0)
